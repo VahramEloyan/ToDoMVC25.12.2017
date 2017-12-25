da4bf5e5e7aa21e8ab3119a54074cff8bf9549fb
vahrameloyan <vahrameloyan@gmail.com>
2017-12-25 15:39:42 +0400

    Added Add and Remove Functions

--- ToDoMVC/src/ToDoMVC.Core/ToDoList.cs
+++ ToDoMVC/src/ToDoMVC.Core/ToDoList.cs
@@ -6,7 +6,19 @@ namespace ToDoMVC.Core
 {
    public class ToDoList
     {
-        public List<ToDoItem> ToDoItems { set; get; }
-        
+        public List<ToDoItem> ToDoItems { set; get; } = new List<ToDoItem>();
+        public void Add(string title)
+        {
+            ToDoItems.Add(new ToDoItem { Title = title });
+        }
+        public bool Remove(int index)
+        {
+            if (index < 0 || index >= ToDoItems.Count)
+            {
+                throw new ArgumentOutOfRangeException("Incorect Argument");               
+            }
+            ToDoItems.RemoveAt(index);
+            return true;
+        }
     }
 }
