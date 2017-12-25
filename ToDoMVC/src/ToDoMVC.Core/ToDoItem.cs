f411775344268665c9a3e54e3e6de7a19c6874db
vahrameloyan <vahrameloyan@gmail.com>
2017-12-25 15:03:58 +0400

    Added Class Library

---
+++ ToDoMVC/src/ToDoMVC.Core/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoMVC.Core
+{
+   public class ToDoItem
+    {
+        public bool IsDone { set; get; }
+        public string Title { set; get; }
+    }
+}
---
+++ ToDoMVC/src/ToDoMVC.Core/ToDoList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoMVC.Core
+{
+   public class ToDoList
+    {
+        public List<ToDoItem> ToDoItems { set; get; }
+        
+    }
+}
